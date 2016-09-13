feb154d7fbaa2f7f530bb792eb04fc954bdc5bc0
Linh Nguyen My <lnguyenmy1@gmail.com>
2016-09-13 23:34:22 +0100

    Added unit_9_virtual_computer.rb file

---
+++ Ruby/unit_9_virtual_computer.rb
@@ -0,0 +1,54 @@
+# Codecademy example 
+
+class Machine
+  @@users = {}
+  
+  def initialize(username, password)
+    @username = username
+    @password = password
+    @@users[username] = password
+    @files = {}
+  end
+  
+  def create(filename)
+    time = Time.now
+    @files[filename] = time
+    puts "#{filename} was created by #{@username} at #{time}."
+  end
+  
+  def Machine.get_users
+    @@users
+  end
+end
+
+my_machine = Machine.new("eric", 01234)
+your_machine = Machine.new("you", 56789)
+
+my_machine.create("groceries.txt")
+your_machine.create("todo.txt")
+
+puts "Users: #{Machine.get_users}"
+
+# My example
+
+class Computer
+    @@users = {}
+    def initialize(username, password)
+        @username = username
+        @password = password
+        @files = {}
+        @@users[username] = password
+    end
+    
+    def create(filename)
+        @@time = Time.now
+        @files[filename] = time
+        puts "Hi #{username}! You've just created #{filename} at #{time}."
+    end
+    
+    def Computer.get_users
+        return @@users
+    end
+end
+
+my_computer = Computer.new(:mike101, :dog123)
